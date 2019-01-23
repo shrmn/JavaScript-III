98e628b065665b2a45eaea72349288c2b0ae25e9
Dan Sherman <dvsherman@protonmail.com>
2019-01-23 14:52:52 -0700

    created GameObject

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,9 +1,12 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+  Object oriented design is commonly used in video games.  
+  For this part of the assignment you will be implementing several constructor 
+  functions with their correct inheritance hierarchy.
 
   In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
 
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  
+  Use the objects at the bottom of the page to test your constructor functions.
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
@@ -14,6 +17,15 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
+
+function GameObject() {
+  this.createdAt = "";
+  this.dimensions = {};  
+}
+
+GameObject.prototype.destroyed = function () {
+  return `Object was removed from the game.`;
+}
 
 /*
   === CharacterStats ===
